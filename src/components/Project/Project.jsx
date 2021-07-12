fb9669bc6ef5c0cab470e5e1b4196cdd72243f13
krehicbenjamin <krehicbenjamin@gmail.com>
2021-07-13 00:46:53 +0200

    Adding project component and it's css

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-
+import './project.css';
 class Project extends Component {
     state = {  }
     render() { 
         return ( 
             <div className="project-container">
-                <img className="project-image" src={this.props.image} alt="" />
                 <h1 className="project-title">
                     {this.props.title}
                 </h1>
+                
+                <img className="project-image" src={this.props.image} alt="" />
+                
                 <p className="projet-description">
                     {this.props.description}
                 </p>
